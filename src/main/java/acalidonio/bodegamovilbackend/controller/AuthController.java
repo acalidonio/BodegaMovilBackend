d60d12144cbf9fc227fa4c95774f7cb00e8ebdeb
acalidonio <116671236+acalidonio@users.noreply.github.com>
2026-07-04 23:23:55 -0600

    Feat: Refactorizar controladores de auth e inventario para utilizar nuevos dtos, agregar paginación y nuevo ensdpoint de restaurar producto

--- src/main/java/acalidonio/bodegamovilbackend/controller/AuthController.java
+++ src/main/java/acalidonio/bodegamovilbackend/controller/AuthController.java
@@ -1,10 +1,10 @@
 package acalidonio.bodegamovilbackend.controller;
 
-import acalidonio.bodegamovilbackend.dto.AuthResponse;
-import acalidonio.bodegamovilbackend.dto.LoginRequest;
+import acalidonio.bodegamovilbackend.domain.dto.response.AuthResponse;
+import acalidonio.bodegamovilbackend.domain.dto.response.GeneralResponse;
+import acalidonio.bodegamovilbackend.domain.dto.request.LoginRequest;
 import acalidonio.bodegamovilbackend.service.AuthService;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
@@ -17,13 +17,13 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
-        try {
-            AuthResponse response = authService.login(request);
-            return ResponseEntity.ok(response);
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-        }
+    public ResponseEntity<GeneralResponse<AuthResponse>> login(@RequestBody LoginRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(
+                GeneralResponse.<AuthResponse>builder()
+                        .message("Inicio de sesión exitoso")
+                        .data(response)
+                        .build()
+        );
     }
 }
-
